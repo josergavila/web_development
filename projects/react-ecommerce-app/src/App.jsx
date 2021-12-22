21b79e97e23032403d01f74a3f58c727178dc7d1
josergavila <jose.avila@vorp.com.br>
2021-12-22 19:13:57 -0300

    started implementation of react ecommerce site

---
+++ projects/react-ecommerce-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Component } from "react/cjs/react.development";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello</h1>
+        <p>io</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default NavBar;
